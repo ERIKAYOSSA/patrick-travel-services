3b75a9a5734f4b3838ad53dd045f27358ae16431
ERIKAYOSSA <erikayossa0507@gmail.com>
2025-08-30 12:39:10 +0100

    correction du fichier auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from app.services.user_service import get_admin_count
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
@@ -12,6 +11,7 @@ async def login_page(request: Request):
 
 @router.get("/register", response_class=HTMLResponse)
 async def register_page(request: Request):
+    from app.services.user_service import get_admin_count
     admin_count = get_admin_count()
     show_statut = admin_count < 3
     return templates.TemplateResponse("register.html", {"request": request, "show_statut": show_statut})
