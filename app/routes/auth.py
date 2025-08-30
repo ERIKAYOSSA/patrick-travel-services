ffca7c754d87a071934ee8d569de2f156574c73a
ERIKAYOSSA <erikayossa0507@gmail.com>
2025-08-30 12:10:15 +0100

    deplacement de get_admin_count dans user_service

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from app.services.user_service import get_admin_count  # si tu l’as déjà défini
+from app.services.user_service import get_admin_count
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -5,3 +5,4 @@ from app.db import get_db  # ou database.py selon ta config
 def get_admin_count():
     db: Session = next(get_db())
     return db.query(User).filter(User.statut == "admin").count()
+
