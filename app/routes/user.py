e0f1c19c816cd2fce07e9484c8b088f0641738dc
ERIKAYOSSA <erikayossa0507@gmail.com>
2025-09-01 11:48:32 +0100

    correction finale

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,12 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form, HTTPException
 from sqlalchemy.orm import Session
 from app.database.connection import SessionLocal
 from app.models.user import User
-from app.services.security import hash_password
-from fastapi import HTTPException
-from app.services.security import verify_password
+from app.services.security import hash_password, verify_password
 from app.services.auth import create_access_token
-from app.schemas.user_schema import UserCreate  # ✅
 
 router = APIRouter()
 
@@ -18,26 +15,45 @@ def get_db():
         db.close()
 
 @router.post("/register")
-def register_user(user: UserCreate, db: Session = Depends(get_db)):
-    new_user = User(
-    username=user.username,
-    email=user.email,
-    password=hash_password(user.password)  # 🔒 mot de passe chiffré
-    )
-    
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return {"message": "Utilisateur enregistré", "id": new_user.id}
+def register_user(
+    username: str = Form(...),
+    email: str = Form(...),
+    password: str = Form(...),
+    statut: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    try:
+        hashed_pw = hash_password(password)
 
+        new_user = User(
+            username=username,
+            email=email,
+            password=hashed_pw,
+            statut=statut,
+            is_admin=(statut == "admin")
+        )
+
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+
+        return {"message": "Utilisateur enregistré", "id": new_user.id}
+
+    except Exception as e:
+        print("Erreur lors de l'enregistrement:", e)
+        raise HTTPException(status_code=500, detail="Erreur interne")
 
 @router.post("/login")
-def login_user(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter(User.email == user.email).first()
+def login_user(
+    email: str = Form(...),
+    password: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    db_user = db.query(User).filter(User.email == email).first()
     if not db_user:
         raise HTTPException(status_code=400, detail="Email incorrect")
 
-    if not verify_password(user.password, db_user.password):
+    if not verify_password(password, db_user.password):
         raise HTTPException(status_code=400, detail="Mot de passe incorrect")
 
     token = create_access_token({"sub": db_user.email})
